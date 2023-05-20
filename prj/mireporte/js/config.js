510d1b45b459ce4c2eb80e012aabd04efb376409
f4gm <fagm032001@gmail.com>
2023-05-19 19:35:31 -0500

    Version preliminar, falta realizar algunos ajustes

---
+++ prj/mireporte/js/config.js
@@ -0,0 +1,60 @@
+// Capas de informacion
+//  Mapas base
+const osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+const esriWorldStreetMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+});
+
+const esriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+//  Capas
+var barrios = L.geoJson(barrios_geoJson);
+
+var reportes = L.geoJson(reportes_geoJson, {
+    onEachFeature: function (feature, layer) {
+        layer.on('mouseover', function(ev) {
+            openPanPoint(ev);
+            attReport(ev.target.feature);
+        });
+    },
+    pointToLayer: function(geoJsonPoint, latlng) {
+        return L.marker(latlng, {icon: reportes});
+    }
+});
+
+const map = L.map('map', {
+    center: reportes.getBounds().getCenter(),
+    zoom: 17,
+    layers: [osm, reportes]
+});
+
+// Grupo de capas
+var mapasBase = {
+    'OpenStreetMaps': osm,
+    'Esri World Street Map': esriWorldStreetMap,
+    'Esri World Imagery': esriWorldImagery
+};
+
+var capas = {
+    'Barrios': barrios,
+    'Reportes': reportes
+};
+
+var grupoCapas = L.control.layers(mapasBase, capas, {collape: 'false'});
+
+var marker = L.marker([0, 0], {icon: markerIcon});
+map.on('contextmenu', mkReport);
+
+map.on('click', removeMarker);
+
+grupoCapas.addTo(map);
+
+L.control.scale({
+    metric: true
+}).addTo(map);
---
+++ prj/mireporte/js/main.js
@@ -0,0 +1,131 @@
+// Clases
+//  Icono de marcadores: Marcadores cuando se seleciona una posición para reportar
+var markerIcon = L.icon({
+    iconUrl: './data/img/icons/mk-report.svg',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+    popupAnchor: [0, -30]
+});
+
+var reportes = L.icon({
+    iconUrl: './data/img/icons/trash.svg',
+    iconSize: [32, 32],
+    iconAnchor: [16, 32]
+});
+
+// Funciones
+//  Constructor de rutas Google Maps: Construye la ruta para llegar al punto indicado con Google Maps
+function googleStreetUrl(lat, lon) {
+    url = "https://www.google.com/maps/dir//" + lat + "," + lon + "/@" + lat + "," + lon + ",19z";
+    return url
+};
+
+//  Constructor de rutas Google Street Maps: Construye la ruta que dirige al punto en Google Street Maps
+function googleViewUrl(lat, lon) {
+    url = "http://maps.google.com/maps?q=&layer=c&cbll=" + lat + "," + lon;
+    return url
+};
+
+//  Abrir panel: Abre el panel de atributos al presionar sobre una feature
+function openPanPoint(event) {
+    var point = document.getElementsByClassName("leaflet-marker-icon")[parseInt(event.target.feature.properties.id) - 1];
+    //punto.setAttribute("role", "button")
+    point.setAttribute("data-bs-toggle", "offcanvas");
+    point.setAttribute("data-bs-target", "#pan");
+    //console.log(punto);
+};
+
+//  Mostrar atributos: Funciones para mostrar los atributos segun la capa de informacion
+//      Atributos de reportes: Muestra los atributos de los reportes anteriores hechos por otros usuarios
+
+//      Atributos de puntos de recogida: Muestra los atributos de las zonas donde se pueden depositar residups
+function attReport(feature) {
+    var title = document.getElementById("panTitle")
+    title.innerHTML = "Identificación del reporte: " + feature.properties.id;
+
+    var content = document.getElementById("panContent");
+    imagePath = "./data/img/reportes/";
+    googleStreet = googleStreetUrl(feature.properties.latitud, feature.properties.longitud);
+    content.innerHTML = "<img id='img' src=" + "'" + imagePath + feature.properties.id + ".webp'" + " class='rounded img-fluid'" + " alt='Imagen del reporte número "+ feature.properties.id + "'>" + 
+        "<p class='p'><strong>Dirección: </strong>" + feature.properties.direccion + "</p>" +
+        "<p class='p'><strong>Clase: </strong>" + feature.properties.clase + "</p>" +
+        "<p class='p'><strong>Contiene: </strong>" + feature.properties.contiene + "</p>" +
+        "<a class='btn btn-success' target='_blanck' href='" + googleStreet + "'role='button'>¿Cómo llegar?</a>" + 
+        "<br><br><a class='btn btn-secondary' target='_blanck' href='" + form(feature.properties.latitud, feature.properties.longitud, 2) + "'role='button'>¿Alguna novedad?</a>";
+};
+
+//      Atrobutos rutas: Muestra los atributos de las rutas de los camión de basura
+
+//      Atributos de barrios: Muestra los atributos de los barrios en el panel
+function attBarrios(feature) {
+    var title = document.getElementById("panTitle")
+    title.innerHTML = "Nombre del barrio: " + feature.properties.barrio;
+
+    var content = document.getElementById("panContent");
+    content.innerHTML = "<p class='lead'> Identificación catastral: " + feature.properties.id_barrio + "</p>";
+};
+
+//  Abrir modal: Abre la ventana con información adicional sobre el punto que se desea reportar
+function openModal(lat, lon) {
+    var body = document.getElementById("body");
+    body.setAttribute("class", "modal-open");
+    body.setAttribute("data-bs-padding-right", "0px");
+
+    var modal = document.getElementById("reporte");
+    modal.removeAttribute("aria-hidden", "true");
+    modal.classList.add('show');
+    modal.setAttribute("aria-modal", "true");
+    modal.setAttribute("role", "dialog");
+    modal.setAttribute("style", "display: block;");
+
+    var streetBtn = document.getElementById("boton-street");
+    streetBtn.setAttribute("href", googleViewUrl(lat, lon));
+    streetBtn.setAttribute("target", "_blanck");
+
+    var divFade = document.createElement("div");
+    divFade.classList.add("offcanvas-backdrop", "fade", "show");
+    divFade.setAttribute("id", "borrar");
+    body.appendChild(divFade);
+};
+
+//  Cerrar modal: Cierra la ventana de reportes y elimina el marcador creado
+function removeMarker(pop) {
+    document.querySelectorAll("#rmMarker").forEach(el => el.remove());
+    if (pop == 1) {
+        document.querySelectorAll(".leaflet-popup").forEach(el => el.remove());
+    }
+};
+
+//  Programacion del reporte
+function mkReport(event) {
+    if (event.latlng.lat != marker._latlng.lat && event.latlng.lng != marker._latlng.lng) {
+        marker.remove();
+        marker = L.marker([event.latlng.lat, event.latlng.lng], {icon: markerIcon});
+        marker.addTo(map);
+        marker.getElement().setAttribute("id", "rmMarker");
+
+        var reportBtn = document.getElementById("reportBtn");
+        var viewBtn = document.getElementById("googleViewBtn");
+        viewBtn.setAttribute("href", googleViewUrl(event.latlng.lat, event.latlng.lng))
+        reportBtn.setAttribute("href", form(event.latlng.lat, event.latlng.lng, 1));
+    };
+    marker.bindPopup('<button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#reportModal">¡Reportar!</button>').openPopup();
+    console.log(event.latlng.lat, event.latlng.lng);
+};
+
+//  Llenar formulario
+function form(lat, lon, type) {
+    // 2 es Actualizar
+    var formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSdY1gN6KXK7TOGcZ82eLM0MS13EYn5iohvkbyotOzJxzyWSWg/viewform?usp=pp_url&';
+    if (type == 1) {
+        // 1 es para un nuevo reporte
+        formUrl += 'entry.2129838264=Nuevo&entry.535134601=' + lat + '&entry.1133370860=' + lon;
+    } else if (type == 2) {
+        // 2 es para actualizar un reporte existente
+        formUrl += 'entry.2129838264=Actualizar&entry.535134601=' + lat + '&entry.1133370860=' + lon;
+    } else {
+        // Si no cumple ninguna devolver el formulario original
+        formUrl = 'https://forms.gle/u1gpFoUgWtAn12cd9';
+    }
+    return formUrl
+};
